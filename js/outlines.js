d46fcd1c8f95dcbef9c2394baa747d6807de6490
Shihua Ma <mashihua@gmail.com>
2011-06-29 12:08:03 +0800

    finish code

--- js/outlines.js
+++ js/outlines.js
@@ -1,45 +1,74 @@
 
-var current_outline = null,
-	current_section = null,
-	stack = [],
-	isHeading = getChecker(/^h[1-6]|hgroup$/i),
-	isSectioningContent = getChecker(/^article|aside|nav|section$/i),
-	isSectioningRoot = getChecker(/^blockquote|body|details|fieldset|figure|td$/i),
-	root = document.body;
-
-
+(function(){
 /*
 Section class
 */
 
 function Section(node){
-	this.sections = [];
-	this.node = node;
-	this.heading = false;
+    this.sections = [];
+    this.node = node;
+    this.heading = false;
 }
 
 Section.prototype.append = function(what){
-		what.container = this;
-		this.sections.push(what);
+    what.container = this;
+    this.sections.push(what);
+}
+
+Section.prototype.toHTML = function(anchor){
+    var value =  headingText(this.heading);
+      if (anchor) {
+          value = '<a href="#'+getId(this.node)+'">'+ value + '</a>';
+      }
+      return value + sectionToHTML(this.sections, anchor);
+
+    return (value=='' ? value : '<ol>'+value+'</ol>');
+}
+
+function headingText(heading){
+    if (isHeading(heading)) {
+        if (getTagName(heading) == 'hgroup') {
+            heading = heading.getElementsByTagName('h'+(-getHeadingRank(heading)))[0];
+        }
+        return heading.textContent || heading.innerText || "<i>No text inside "+heading.nodeName+"</i>";
+    }
+    return ""+haeding;
+}
+
+function sectionToHTML(sections, anchor){
+    var value = '';
+    for (var i=0; i < sections.length; i++) {
+        value += '<li>'+sections[i].toHTML(anchor)+'</li>';
+    }
+    return (value=='' ? value : '<ol>'+value+'</ol>');
 }
 
 
+function getId(ele){
+    var id = ele.getAttribute('id');
+    if (id){
+       return id; 
+    }
+    id = 'outlines-' + (Math.random() * (1 << 16)).toString(8).replace('.', '');
+    ele.setAttribute('id', id);
+    return id;
+}
 /*
 get function to check element is in a categories
 */
 
 function getChecker(reg){
-	return function(ele){
-		return ele && ele.tagName && reg.test(getTagName(ele));
-	}
+    return function(ele){
+        return ele && ele.tagName && reg.test(getTagName(ele));
+    }
 }
 
 /*
-get lower case wlwmwnt tag name
+get lower case element tag name
 */
 
 function getTagName(ele){
-	return ele.tagName.toLowerCase(); 
+    return ele.tagName.toLowerCase(); 
 }
 
 /*
@@ -47,16 +76,16 @@ get highest-ranked for heading content element.
 */
 
 function getHeadingRank(ele){
-	var name = getTagName(ele);
-	if(name === 'hgroup'){
-		for (var i = 1; i <= 6; i++) {
-			if (el.getElementsByTagName('H' + i).length > 0) {
-	  		return i;
-	  	}
-		}
-	}else{
-		return paeseInt(name.substr(1));
-	}
+    var name = getTagName(ele);
+    if(name === 'hgroup'){
+        for (var i = 1; i <= 6; i++) {
+            if (ele.getElementsByTagName('H' + i).length > 0) {
+                return -i;
+            }
+        }
+    }else{
+        return -parseInt(name.substr(1));
+    }
 }
 
 /*
@@ -64,7 +93,7 @@ get last section from sections
 */
 
 function getLastSection(sections){
-	return sections[sections.length - 1];
+    return sections[sections.length - 1];
 }
 
 
@@ -72,124 +101,131 @@ function getLastSection(sections){
 get rank form section
 */
 function sectionHeadingRank(section){
-	var heading = section.heading;
-	return isHeading(heading) 
-					? getHeadingRank(heading) 
-					: 1; 
+    var heading = section.heading;
+    return isHeading(heading) 
+                    ? getHeadingRank(heading) 
+                    : 1; 
 }
 
 
-
+var current_outline = null,
+    current_section = null,
+    stack = [],
+    isHeading = getChecker(/^h[1-6]|hgroup$/i),
+    isSectioningContent = getChecker(/^article|aside|nav|section$/i),
+    isSectioningRoot = getChecker(/^blockquote|body|details|fieldset|figure|td$/i);
 
 /*
 entering an element
 */
 
 function enter(ele){
-	
-	//If the top of the stack is a heading content element,do nothing.
-	if(isHeading(stack[stack.length - 1])){
-		return;
-	}
-	
-	//When entering a sectioning content element or a sectioning root element
-	if (isSectioningContent(ele) || isSectioningRoot(ele)) {
-		
-		//If current outlinee is not null, and the current section has no heading, 
-		if (current_outline !== null && !current_section.heading) {
-			
-				//create an implied heading,let that be the heading for the current section.
-				current_section.heading = current_section;
-		}
-			
-		//If current outlinee is not null, push current outlinee onto the stack.
-  	if (current_outline !== null) {
-  		stack.push(current_outline);
-  	}
-		
-		//Let current outlinee be the element that is being entered.
-		current_outline = ele;
-		
-		//Let current section be a newly created section for the current outlinee element.
-		current_section = new Section(ele);
-		
-		//Let there be a new outline for the new current outlinee, 
-		//initialized with just the new current section as the only section in the outline.
-  	current_outline.outline = {sections: [current_section], node: ele};
-		
-		return;
-	}
-	
-	//If the current outlinee is null,do nothing
-	if (current_outline === null){
-		return;
-	}
-	
-	// When entering a heading content element
-	if (isHeading(ele)) {
-		
-		//If the current section has no heading, let the element being entered be the heading for the current section.
-		if (!current_section.heading) {
-	
-		current_section.heading = ele;
-		
-		// Otherwise, if the element being entered has a rank equal to or greater than the heading of the last section of the outline of the current outlinee, 
-		}else if (getHeadingRank(ele) >= getHeadingRank(getLastSection(current_section.outline.sections)) {
-		
-			// create a new section 
-			var section = new Section(ele);
-			
-			//append it to the outline of the current outlinee element
-			current_outline.outline.sections.push(section);
-			
-			//Let current section be that new section. 
-			current_section = section;
-			
-			//Let the element being entered be the new heading for the current section.
-			current_section.heading = ele;
-			
-		// Otherwise, run these substeps:
-		} else {
-			
-			var abort = false;
-			
-			//Let candidate section be current section. 
-			var candidate_section = current_section;
-				
-			do{				
-				//If the element being entered has a rank  lower than the rank of the heading of the candidate section
-				if (getHeadingRank(ele) < sectionHeadingRank(candidate_section)) {
-					
-					//append it to candidate section.
-					var section = new Section(ele);
-	
-					// and append it to candidate section.(This does not change which section is the last section in the outline.)
-					candidate_section.append(section);
-					
-					// Let current section be this new section.
-					current_section = section;
-					
-					// Let the element being entered be the new heading for the current section.
-					current_section.heading = ele;
-					
-					// Abort these substeps.
-					abort = true;
-		  	}
-				
-				// Let new candidate section be the section that contains candidate section in the outline of current outlinee.
-				var new_candidate_section = candidate_section.container;
-					
-				//Let candidate section be new candidate section.
-				candidate_section = new_candidate_section;
-					
-			//Return to step 2.
-			}while(!abort)
-			
-		}
-		// Push the element being entered onto the stack.
-		stack.push(ele);
-	}
-	//Otherwise,do nothing. 
+    
+    //If the top of the stack is a heading content element,do nothing.
+    if(isHeading(stack[stack.length - 1])){
+        return;
+    }
+    
+    //When entering a sectioning content element or a sectioning root element
+    if (isSectioningContent(ele) || isSectioningRoot(ele)) {
+        //If current outlinee is not null, and the current section has no heading, 
+        if (current_outline !== null && !current_section.heading) {
+    
+            //create an implied heading,let that be the heading for the current section.
+            current_section.heading = current_section;
+        }
+
+        //If current outlinee is not null, push current outlinee onto the stack.
+        if (current_outline !== null) {
+          stack.push(current_outline);
+        }
+        
+        //Let current outlinee be the element that is being entered.
+        current_outline = ele;
+
+        //Let current section be a newly created section for the current outlinee element.
+        //Associate current outlinee with current section.
+        current_section = new Section(ele);
+        //Let there be a new outline for the new current outlinee, 
+        //initialized with just the new current section as the only section in the outline.
+        current_outline.outline = {
+        sections: [current_section], 
+        node: ele , 
+        toHTML:function(anchor){
+            return sectionToHTML(this.sections,anchor);
+            }
+        };
+        return;
+    }
+    
+    //If the current outlinee is null,do nothing
+    if (current_outline === null){
+        return;
+    }
+    
+    // When entering a heading content element
+    if (isHeading(ele)) {
+
+        //If the current section has no heading, let the element being entered be the heading for the current section.
+        if (!current_section.heading) {
+    
+        current_section.heading = ele;
+        
+        // Otherwise, if the element being entered has a rank equal to or greater than the heading of the last section of the outline of the current outlinee, 
+        }else if (getHeadingRank(ele) >= sectionHeadingRank(getLastSection(current_outline.outline.sections))){
+        
+            // create a new section 
+            var section = new Section(ele);
+            
+            //append it to the outline of the current outlinee element
+            current_outline.outline.sections.push(section);
+            
+            //Let current section be that new section. 
+            current_section = section;
+            
+            //Let the element being entered be the new heading for the current section.
+            current_section.heading = ele;
+        // Otherwise, run these substeps:
+        } else {
+            
+            //Let candidate section be current section. 
+            var abort = false,
+                candidate_section = current_section;
+                
+            do{
+                //If the element being entered has a rank  lower than the rank of the heading of the candidate section
+                if (getHeadingRank(ele) < sectionHeadingRank(candidate_section)) {
+                    
+                    //append it to candidate section.
+                    var section = new Section(ele);
+    
+                    // and append it to candidate section.(This does not change which section is the last section in the outline.)
+                    candidate_section.append(section);
+                    
+                    // Let current section be this new section.
+                    current_section = section;
+                    
+                    // Let the element being entered be the new heading for the current section.
+                    current_section.heading = ele;
+                    
+                    // Abort these substeps.
+                    abort = true;
+                }
+                
+                // Let new candidate section be the section that contains candidate section in the outline of current outlinee.
+                var new_candidate_section = candidate_section.container;
+                    
+                //Let candidate section be new candidate section.
+                candidate_section = new_candidate_section;
+                    
+            //Return to step 2.
+            }while(!abort)
+            
+        }
+        // Push the element being entered onto the stack.
+        stack.push(ele);
+    }
+    //Otherwise,do nothing. 
 }
 
 
@@ -198,66 +234,64 @@ exiting an element
 */
 
 function exit(ele){
-	
-	// If the top of the stack is an element, and you are exiting that element
-	// 		Note:The element being exited is a heading content element.
-	// Pop that element from the stack.
-	var stackTop = stack[stack.length - 1];
-	if (isHeading(stackTop)) {
-		if (stackTop == ele) {
-			stack.pop();
-		}
-		return;
-	}
-	
-	//When exiting a sectioning content element, if the stack is not empty
-	if (isSectionContent(ele) && stack.length > 0) {
-		
-		//Pop the top element from the stack, and let the current outlinee be that element.
-		current_outline = stack.pop();
-		
-		//Let current section be the last section in the outline of the current outlinee element.
-		current_section = getLastSection(current_outline.outline.section);
-			
-		// Append the outline of the sectioning content element being exited to the current section. (This does not change which section is the last section in the outline.)
-		for (var i = 0, sections = ele.outline.sections, len = sections.length; i < len; i++) {
-			current_section.append(sections[i]);
-		}
-		return;
+    
+    // If the top of the stack is an element, and you are exiting that element
+    //         Note:The element being exited is a heading content element.
+    // Pop that element from the stack.
+    var stackTop = stack[stack.length - 1];
+    if (isHeading(stackTop)) {
+        if (stackTop == ele) {
+            stack.pop();
+        }
+        return;
+    }
+    
+    //When exiting a sectioning content element, if the stack is not empty
+    if (isSectioningContent(ele) && stack.length > 0) {
+        
+        //Pop the top element from the stack, and let the current outlinee be that element.
+        current_outline = stack.pop();
+        
+        //Let current section be the last section in the outline of the current outlinee element.
+        current_section = getLastSection(current_outline.outline.sections);
+            
+        // Append the outline of the sectioning content element being exited to the current section. (This does not change which section is the last section in the outline.)
+        for (var i = 0, sections = ele.outline.sections, len = sections.length; i < len; i++) {
+            current_section.append(sections[i]);
+        }
+        return;
   }
-	
-	//When exiting a sectioning root element, if the stack is not empty
-	if( isSectionRoot(ele) && stack.length ){
-		
-		//Pop the top element from the stack, and let the current outlinee be that element. 
-		current_outline = stack.pop();
-		
-		//Let current section be the last section in the outline of the current outlinee element. 
-		current_section = getLastSection(current_outline.outline.section);
-		
-		//Finding the deepest child: If current section has no child sections, stop these steps. 
-		while(current_section.length){
-						
-			// Let current section be the last child section of the current current section.
-			current_section = getLastSection(current_section);
-				
-		// Go back to the substep labeled finding the deepest child.
-		}
-		
-	}
-	
-	// When exiting a sectioning content element or a sectioning root element
-	// 	Note:The current outlinee is the element being exited.
-	if (isSectionConttent(ele) || isSectionRoot(ele)) {
-		
-		// Let current section be the first section in the outline of the current outlinee element.
-		current_section = current_section.outline.sections[0];
-		 
-		// Skip to the next step in the overall set of steps. (The walk is over.)
-		return;
-	}
-	
-	// If the current outlinee is null, do nothing
+    
+    //When exiting a sectioning root element, if the stack is not empty
+    if( isSectioningRoot(ele) && stack.length ){
+        
+        //Pop the top element from the stack, and let the current outlinee be that element. 
+        current_outline = stack.pop();
+        //Let current section be the last section in the outline of the current outlinee element. 
+        current_section = getLastSection(current_outline.outline.sections);
+        
+        //Finding the deepest child: If current section has no child sections, stop these steps. 
+        while(current_section.length){
+                        
+            // Let current section be the last child section of the current current section.
+            current_section = getLastSection(current_section);
+                
+        // Go back to the substep labeled finding the deepest child.
+        }
+        
+    }
+    
+    // When exiting a sectioning content element or a sectioning root element
+    //     Note:The current outlinee is the element being exited.
+    if (isSectioningContent(ele) || isSectioningRoot(ele)) {
+        // Let current section be the first section in the outline of the current outlinee element.
+        current_section = current_outline.outline.sections[0];
+         
+        // Skip to the next step in the overall set of steps. (The walk is over.)
+        return;
+    }
+    
+    // If the current outlinee is null, do nothing
 }
 
 
@@ -280,11 +314,23 @@ function treeNodeWalker(root, enter, exit) {
         continue start;
       }
       if (node == root) {
-	  		node = null;
-		  }else {
-		  	node = node.parentNode;
-		  }
+              node = null;
+          }else {
+              node = node.parentNode;
+          }
     }
   }
 }
 
+
+function HTMLOutline(root){
+    current_outline = null,current_section = null,stack = [];
+    treeNodeWalker(root,enter,exit);
+    return current_outline ? current_outline.outline : null;;
+}
+
+if(this.exports != undefined)
+    exports.HTMLOutline = HTMLOutline;
+else
+    this.HTMLOutline = HTMLOutline;
+}());
--- test/spec/outlines_spec.js
+++ test/spec/outlines_spec.js
@@ -1,30 +1,162 @@
 #!/usr/bin/env node
 
 var http = require('http'),
-	sys = require('sys'),
-	request = require('request'),
-	HTML5 = require('html5'),
-  Script = process.binding('evals').Script,
-  fs = require('fs'),
-  jsdom = require('jsdom'),
-  window = jsdom.jsdom(null, null, {parser: HTML5}).createWindow(),
-	parser = new HTML5.Parser({document: window.document});
-	
-parser.parse("<body><h1>Page title</h1><hgroup><h3>Subheading</h3><h2>Test</h2></hgroup><h1>One more H1</h1></body>");
-console.log(HTML5Outline(window.document.body).sections);
-/*
-describe("array test", function() {
-	it('test Array.prototype.indexOf function with return value -1',function(){
-			expect(-1).toEqual([1,2,3,4,5,6].indexOf(9));
-	});
-	
-	it('test Array.prototype.indexOf function',function(){
-			expect(3).toEqual([1,2,3,4,5,6].indexOf(4));
-	});
+    sys = require('sys'),
+    HTML5 = require('html5'),
+    jsdom = require('jsdom');
 
-	it('test Array.prototype.indexOf function with formIndex',function(){
-			expect(4).toEqual([1,2,3,4,5,6].indexOf(5,3));
-	});
+describe("Headings and sections test", function() {
+    var window = jsdom.jsdom(null, null, {parser: HTML5}).createWindow(),
+        parser = new HTML5.Parser({document: window.document}),
+        outlines;
+    parser.parse("<body><h1>Foo</h1><h2>Bar</h2><blockquote><h3>Bla</h3></blockquote><p>Baz</p><h2>Quux</h2><section><h3>Thud</h3></section><p>Grunt</p></body>");
+    outlines = HTMLOutline(window.document.body);
+    
+    it('should contant 1 section',function(){
+        expect(1).toEqual(outlines.sections.length);
+    });
+    
+    it('the section 1 should 3 sections ',function(){
+        expect(3).toEqual(outlines.sections[0].sections.length);
+    });
+    
+    it('this should return h2',function(){
+        expect('h2').toEqual(outlines.sections[0].sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('this should return h3',function(){
+        expect('h3').toEqual(outlines.sections[0].sections[2].heading.tagName.toLowerCase());
+    });
+    
+    it('the outline shoud equal',function(){
+        expect('<ol><li>Foo<ol><li>Bar</li><li>Quux</li><li>Thud</li></ol></li></ol>').toEqual(outlines.toHTML());
+    });
 });
+
+
+
+
+describe("hgroup test", function() {
+    var window = jsdom.jsdom(null, null, {parser: HTML5}).createWindow(),
+        parser = new HTML5.Parser({document: window.document}),
+        outlines;
+    parser.parse("<body><h1>H1</h1><hgroup><h3>Subheading</h3><h2>H2</h2></hgroup><h1>Other H1</h1></body>");
+    outlines = HTMLOutline(window.document.body);
+    
+    it('should contant 2 section',function(){
+        expect(2).toEqual(outlines.sections.length);
+    });
+    
+    it('the section 1 should return h1 ',function(){
+        expect('h1').toEqual(outlines.sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the section 1 of section 1 should return hgroup ',function(){
+        expect('hgroup').toEqual(outlines.sections[0].sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the outline shoud equal',function(){
+        expect('<ol><li>H1<ol><li>H2</li></ol></li><li>Other H1</li></ol>').toEqual(outlines.toHTML());
+    });
+});
+
+
+
+describe("implied section test", function() {
+    var window = jsdom.jsdom(null, null, {parser: HTML5}).createWindow(),
+        parser = new HTML5.Parser({document: window.document}),
+        outlines;
+    parser.parse("<body><h1>A</h1><p>B</p><h2>C</h2><p>D</p><h2>E</h2><p>F</p></body>");
+    outlines = HTMLOutline(window.document.body);
+    
+    it('should contant 1 section',function(){
+        expect(1).toEqual(outlines.sections.length);
+    });
+    
+    it('the section 1 should return h1 ',function(){
+        expect('h1').toEqual(outlines.sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the section 1 of section 1 should return h2 ',function(){
+        expect('h2').toEqual(outlines.sections[0].sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the outline shoud equal',function(){
+        expect('<ol><li>A<ol><li>C</li><li>E</li></ol></li></ol>').toEqual(outlines.toHTML());
+    });
+});
+
+describe("one more implied section test", function() {
+    var window = jsdom.jsdom(null, null, {parser: HTML5}).createWindow(),
+        parser = new HTML5.Parser({document: window.document}),
+        outlines;
+    parser.parse("<body><h4>Apples</h4><p>Apples are fruit.</p><section><h2>Taste</h2><p>They taste lovely.</p><h6>Sweet</h6><p>Red apples are sweeter than green ones.</p><h1>Color</h1><p>Apples come in various colors.</p></section></body>");
+    outlines = HTMLOutline(window.document.body);
+    
+    it('should contant 1 section',function(){
+        expect(1).toEqual(outlines.sections.length);
+    });
+    
+    it('the section 1 should return h4',function(){
+        expect('h4').toEqual(outlines.sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the section 1 of section 1 should return h2 ',function(){
+        expect('h2').toEqual(outlines.sections[0].sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the section 1 of section 1 should have one section',function(){
+        expect(1).toEqual(outlines.sections[0].sections[0].sections.length);
+    });
+    
+    it('the outline shoud equal',function(){
+        expect('<ol><li>Apples<ol><li>Taste<ol><li>Sweet</li></ol></li><li>Color</li></ol></li></ol>').toEqual(outlines.toHTML());
+    });
+});
+
+describe("one more implied section test", function() {
+    var window = jsdom.jsdom(null, null, {parser: HTML5}).createWindow(),
+        parser = new HTML5.Parser({document: window.document}),
+        outlines;
+    parser.parse("<body><h4>Apples</h4><p>Apples are fruit.</p><section><h2>Taste</h2><p>They taste lovely.</p><h6>Sweet</h6><p>Red apples are sweeter than green ones.</p><h1>Color</h1><p>Apples come in various colors.</p></section></body>");
+    outlines = HTMLOutline(window.document.body);
+    
+    it('should contant 1 section',function(){
+        expect(1).toEqual(outlines.sections.length);
+    });
+    
+    it('the section 1 should return h4',function(){
+        expect('h4').toEqual(outlines.sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the section 1 of section 1 should return h2 ',function(){
+        expect('h2').toEqual(outlines.sections[0].sections[0].heading.tagName.toLowerCase());
+    });
+    
+    it('the section 1 of section 1 should have one section',function(){
+        expect(1).toEqual(outlines.sections[0].sections[0].sections.length);
+    });
+    
+    it('the outline shoud equal',function(){
+        expect('<ol><li>Apples<ol><li>Taste<ol><li>Sweet</li></ol></li><li>Color</li></ol></li></ol>').toEqual(outlines.toHTML());
+    });
+});
+/*
+<section>   
+  <h1>Forest elephants</h1>    
+  <section>     
+    <h2>Introduction</h2>     
+    <p>In this section, we discuss the lesser known forest elephants
+  </section>   
+  <section>     
+    <h2>Habitat</h2>
+    <P>Forest elephants do not live in trees but among them. 
+  </section>
+   <aside>
+    <p>advertising block
+  </aside>
+</section>
+<footer>
+  <p>(c) 2010 The Example company
+</footer>
 */
-//sys.puts(window.document.innerHTML);
